384431f488cf8532d0985b22fcfcba10e82b00a8
AdhocMaster <adhocmaster@live.com>
2017-07-25 14:16:50 +0600

    test

--- Project3/Program.cs
+++ Project3/Program.cs
@@ -14,19 +14,28 @@ namespace SharePc
 
              SharePC pc1 = new SharePC();             
              
-             pc1.shareControl();
-             String viewInvitation = pc1.getInvitationString(5); //the max no of client 
-             Console.WriteLine("for control:\n"+viewInvitation);
+             //pc1.shareControl();
+             //String viewInvitation = pc1.getInvitationString(5); //the max no of client 
+             //Console.WriteLine("for control:\n"+viewInvitation);
+
+             //pc1.disconnect();
+             
+             //pc1.shareControl();
+             //String controlInvitation = pc1.getInvitationString( ( int ) 2 );
+             //Console.WriteLine("for control:\n"+controlInvitation);
+
+
+             String invitation = pc1.GetControlString();
 
              pc1.disconnect();
-             
-             pc1.shareControl();
-             String controlInvitation = pc1.getInvitationString( ( int ) 2 );
-             Console.WriteLine("for control:\n"+controlInvitation);
-             
+
+             invitation = pc1.GetControlString();
+
              String a = Console.ReadLine();
 
-             System.Diagnostics.Debug.Print( controlInvitation );
+             System.Diagnostics.Debug.Print( invitation );
+
+             pc1.destroy();
 
              
          }
